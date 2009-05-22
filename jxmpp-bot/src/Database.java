104e3f7abef9d341ee117214f7dad3329ecf1ea9
ternovich.work <ternovich.work@3b9d0e60-468d-11de-a2ae-350cbd0950bb>
2009-05-22 13:00:43 +0000

    minor changes to Database class

--- jxmpp-bot/src/Database.java
+++ jxmpp-bot/src/Database.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.io.*;
 
@@ -22,43 +23,81 @@ public class Database {
 	this.fileName = fileName;
     }
     
-    public boolean InsertUser(String realName, String JID, AccessLevel accessLevel ){
-	boolean result = false;
-	
-	try {
-	    
-	    Connection conn = getConnection();
-	    
-	    PreparedStatement prep = conn.prepareStatement(
-		    "insert into users(real_name,access_level) values (?,?);");
-	    prep.setString(1, realName);
-	    prep.setInt(2, accessLevel.getValue());
-	    
-	    System.out.print(prep.execute());
-	    prep.close();
+    public boolean InsertUser(String realName, String JID,
+			AccessLevel accessLevel) {
+		boolean result = false;
 
-	    //Statement stat = conn.createStatement();
-	    //System.out.print(stat.execute("insert into users(real_name,access_level) values('1','2')"));
-	    
-	    conn.close();
-	    
-	} catch (Exception e) {
-    	    System.out.print(e.getMessage());
+		Connection conn = null;
+		PreparedStatement prep = null;
+		
+		try {
+			conn = getConnection();
+
+			prep = 
+				conn.prepareStatement("insert into users(real_name,access_level) values (?,?);");
+			
+			prep.setString(1, realName);
+			prep.setInt(2, accessLevel.getValue());
+			
+			result = prep.execute();
+
+			prep.close();
+			
+			//TODO: Select inserted user id and insert userJID into jids table 
+			
+		} catch (Exception e) {
+			CloseConnection(conn);
+		}
+
+		return result;
 	}
-	
-	return result;
+    
+    public boolean InsertUserJid(long UserID, String JID){
+    	boolean result = false;
+    	
+    	Connection conn = null;
+    	PreparedStatement prep = null;
+    	
+    	try {
+    		conn = getConnection();
+    		
+    		prep = 
+    			conn.prepareStatement("insert into jids(id_user,jid) values(?,?);");
+    		prep.setLong(1, UserID);
+    		prep.setString(2, JID);
+    		
+    		result = prep.execute();
+    		
+    		prep.close();
+			
+		} catch (Exception e) {
+			CloseConnection(conn);
+		}
+    	
+    	return result;
     }
     
-    protected Connection getConnection(){
-	Connection result = null;
-	
-	try {
-	    Class.forName("org.sqlite.JDBC");
-	    result = DriverManager.getConnection("jdbc:sqlite:" + fileName);
-	} catch (Exception e) {
-	    result = null;
+    protected Connection getConnection() {
+		Connection result = null;
+
+		try {
+			Class.forName("org.sqlite.JDBC");
+			result = DriverManager.getConnection("jdbc:sqlite:" + fileName);
+		} catch (Exception e) {
+			result = null;
+		}
+
+		return result;
 	}
-	
-	return result;
+    protected void CloseConnection(Connection conn){
+    	if ( conn != null){
+    		try {
+				conn.close();
+			} catch (Exception e) {}
+    	}
+    }
+    
+    protected long GetLastRecordID(){
+    	return -1;
     }
 }
