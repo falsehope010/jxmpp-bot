f3a0534343c764389efaa16ae863a0aa26608309
ternovich.work <ternovich.work@3b9d0e60-468d-11de-a2ae-350cbd0950bb>
2009-06-08 10:42:03 +0000

    Helper methods for Database class

---
+++ jxmpp-bot/src/exceptions/DatabaseSequenceNotFoundException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class DatabaseSequenceNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5615692686298903326L;
+
+}
