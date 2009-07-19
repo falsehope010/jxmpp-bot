f7744ece22b575161fe0be3b45efb9f236ba821f
ternovich.work <ternovich.work@3b9d0e60-468d-11de-a2ae-350cbd0950bb>
2009-07-19 06:28:43 +0000

    Removed unnecessary locks in utils

--- jxmpp-bot/src/utils/StackTraceUtil.java
+++ jxmpp-bot/src/utils/StackTraceUtil.java
@@ -4,23 +4,19 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 
 public class StackTraceUtil {
-	private StackTraceUtil() {
-		// nothing todo here
+    private StackTraceUtil() {
+	// nothing todo here
+    }
+
+    public static String toString(Throwable e) {
+	StringWriter sw = new StringWriter();
+	PrintWriter pw = new PrintWriter(sw, false);
+
+	if (e != null) {
+	    e.printStackTrace(pw);
+	    pw.flush();
 	}
 
-	public static String toString(Throwable e) {
-		synchronized (guard) {
-			StringWriter sw = new StringWriter();
-			PrintWriter pw = new PrintWriter(sw, false);
-
-			if (e != null) {
-				e.printStackTrace(pw);
-				pw.flush();
-			}
-
-			return sw.toString();
-		}
-	}
-
-	private static Object guard = new Object();
+	return sw.toString();
+    }
 }
