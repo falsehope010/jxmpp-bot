a03628f40ec666b670c6b04cd7c258ad43cb7c08
ternovich.work <ternovich.work@3b9d0e60-468d-11de-a2ae-350cbd0950bb>
2009-07-19 06:28:43 +0000

    Removed unnecessary locks in utils

--- trunk/jxmpp-bot/src/utils/RandomUtils.java
+++ trunk/jxmpp-bot/src/utils/RandomUtils.java
@@ -4,26 +4,23 @@ import java.util.Random;
 
 public class RandomUtils {
 
-	public static String getRandomString(int length) {
-		synchronized (guard) {
-			if (length <= 0)
-				throw new IllegalArgumentException();
+    public static String getRandomString(int length) {
+	if (length <= 0)
+	    throw new IllegalArgumentException();
 
-			StringBuilder sb = new StringBuilder();
+	StringBuilder sb = new StringBuilder();
 
-			for (int i = 0; i < length; ++i) {
-				int index = rnd.nextInt(maxChars);
-				sb.append(chars.charAt(index));
-			}
-
-			return sb.toString();
-		}
+	for (int i = 0; i < length; ++i) {
+	    int index = rnd.nextInt(maxChars);
+	    sb.append(chars.charAt(index));
 	}
 
-	private static final Random rnd = new Random();
-	private static final String chars = "abcdefghijklmnopqrstuvwxyz"
-			+ "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-			+ "01234567890!@#$%^&*()_+=-,./<>?:;[]{}";
-	private static final int maxChars = chars.length();
-	private static Object guard = new Object();
+	return sb.toString();
+    }
+
+    private static final Random rnd = new Random();
+    private static final String chars = "abcdefghijklmnopqrstuvwxyz"
+	    + "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	    + "01234567890!@#$%^&*()_+=-,./<>?:;[]{}";
+    private static final int maxChars = chars.length();
 }
