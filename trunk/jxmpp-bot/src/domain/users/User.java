ee66d00fae652fabac8d939950019aebc50e5174
ternovich.work <ternovich.work@3b9d0e60-468d-11de-a2ae-350cbd0950bb>
2009-07-21 19:47:28 +0000

    Updated database model. Started UserMapper and modified docs

--- trunk/jxmpp-bot/src/domain/users/User.java
+++ trunk/jxmpp-bot/src/domain/users/User.java
@@ -1,195 +1,54 @@
 package domain.users;
 
-import java.util.ArrayList;
+import java.util.Date;
 
 import domain.DomainObject;
 
 public class User extends DomainObject {
-	String _real_name;
-	AccessLevel _accessLevel;
-	ArrayList<String> _jidCollection;
 
-	/**
-	 * Creates new User
-	 * 
-	 * @param realName
-	 *            User real name (any combination of name, surname, lastname)
-	 * @param jid
-	 *            User JID (jabber identifier)
-	 * @param accessLevel
-	 *            Access level of user (affects permissions)
-	 * @throws NullPointerException
-	 *             Thrown if accessLevel or jid parameter is null
-	 */
-	public User(String realName, String jid, AccessLevel accessLevel)
-			throws NullPointerException {
+    public String getRealName() {
+	return realName;
+    }
 
-		if (accessLevel == null) {
-			throw new NullPointerException("accessLevel can't be null");
-		}
-		if (jid == null) {
-			throw new NullPointerException("jid can't be null");
-		}
+    public String getJob() {
+	return job;
+    }
 
-		mapperSetPersistence(false);
-		_real_name = realName;
+    public String getPosition() {
+	return position;
+    }
 
-		_accessLevel = accessLevel;
+    public Date getBirthday() {
+	return birthday;
+    }
 
-		_jidCollection = new ArrayList<String>();
-		_jidCollection.add(jid);
-	}
+    public String getComments() {
+	return comments;
+    }
 
-	// getters
+    public void setRealName(String realName) {
+	this.realName = realName;
+    }
 
-	/**
-	 * Gets user real name. Can be null
-	 * 
-	 * @return User's real name (any combination of name, surname, last name and
-	 *         so on
-	 */
-	public String getRealName() {
-		return _real_name;
-	}
+    public void setJob(String job) {
+	this.job = job;
+    }
 
-	/**
-	 * Gets user access level. Can't be null
-	 * 
-	 * @return User access level
-	 */
-	public AccessLevel getAccessLevel() {
-		return _accessLevel;
-	}
+    public void setPosition(String position) {
+	this.position = position;
+    }
 
-	/**
-	 * Sets new user real name. Can be null
-	 * 
-	 * @param realName
-	 *            New user's real name
-	 */
-	public void setRealName(String realName) {
-		_real_name = realName;
-	}
+    public void setBirthday(Date birthday) {
+	this.birthday = birthday;
+    }
 
-	/**
-	 * Sets new user access level. Can't be null
-	 * 
-	 * @param accessLevel
-	 *            New user's access level.
-	 * @throws NullPointerException
-	 *             Thrown if you passed null accessLevel parameter
-	 */
-	public void setAccessLevel(AccessLevel accessLevel)
-			throws NullPointerException {
+    public void setComments(String comments) {
+	this.comments = comments;
+    }
 
-		if (accessLevel == null) {
-			throw new NullPointerException("accessLevel can't be null");
-		}
-
-		_accessLevel = accessLevel;
-	}
-
-	/**
-	 * Gets collection of JIDs for user
-	 * 
-	 * @return JID collection of user
-	 */
-	public ArrayList<String> getJidCollection() {
-		return _jidCollection;
-	}
-
-	/**
-	 * Gets jid count. User can have multiple jids
-	 * 
-	 * @return Jid count
-	 */
-	public int getJidCount() {
-		return _jidCollection.size();
-	}
-
-	/**
-	 * Adds JID to user's jid collection. Duplicates will be ignored
-	 * 
-	 * @param JID
-	 *            JID to be added into user's jid collection. Can't be null
-	 * @throws NullPointerException
-	 *             Thrown if JID to be added is null
-	 */
-	public void addJID(String JID) throws NullPointerException {
-
-		if (JID == null)
-			throw new NullPointerException("JID can't be null reference");
-
-		if (!_jidCollection.contains(JID)) {
-			_jidCollection.add(JID);
-		}
-	}
-
-	/**
-	 * Removes JID from user's jid collection.
-	 * 
-	 * @param JID
-	 *            JID to be removed, can be null
-	 */
-	public void removeJID(String JID) {
-		if (_jidCollection.contains(JID)) {
-			_jidCollection.remove(JID);
-		}
-	}
-
-	/**
-	 * Checks whether user has given JID
-	 * 
-	 * @param JID
-	 *            JID to be checked
-	 * @return True if user has given JID, false otherwise
-	 */
-	public boolean hasJID(String JID) {
-		return _jidCollection.contains(JID);
-	}
-
-	public void DebugPrint() {
-		System.out.print("User id: " + getID() + '\n');
-		System.out.print("User name: " + _real_name + '\n');
-		System.out.print("User access level: " + _accessLevel.value + '\n');
-
-		for (String jid : _jidCollection) {
-			System.out.print("   " + jid + '\n');
-		}
-	}
-
-	/**
-	 * Checks two users for equality
-	 * 
-	 * @param rhs
-	 *            User with which equality will be checked
-	 * @return True if users are equal, false otherwise
-	 */
-	public boolean equals(User rhs) {
-		boolean result = false;
-
-		try {
-
-			if (getID() == rhs.getID() && _real_name.equals(rhs.getRealName())) {
-				int rhs_level = rhs.getAccessLevel().getValue();
-
-				if (_accessLevel.getValue() == rhs_level) {
-
-					ArrayList<String> rhsJidCollection = rhs.getJidCollection();
-					// compare jids
-					if (getJidCount() == rhsJidCollection.size()) {
-
-						// compare each JID one by one
-						result = (_jidCollection.containsAll(rhsJidCollection) && _jidCollection
-								.containsAll(rhsJidCollection));
-					}
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return result;
-	}
-
+    String realName;
+    String job;
+    String position;
+    Date birthday;
+    String comments;
 }
