1359d814e8c34f15691a8d71f1d1fbf1637ec23b
ternovich.work <ternovich.work@3b9d0e60-468d-11de-a2ae-350cbd0950bb>
2009-07-22 13:51:24 +0000

    UserMapper implemented save()

--- trunk/jxmpp-bot/src/mappers/UserMapper.java
+++ trunk/jxmpp-bot/src/mappers/UserMapper.java
@@ -22,24 +22,76 @@ public class UserMapper extends AbstractMapper {
 	return false;
     }
 
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This implementation performs mapping of {@link User} into database.
+     * <p>
+     * If record isn't persistent it is simply inserted into corresponding
+     * database table. If record is persistent it is updated. No additional
+     * business logic is performed.
+     * <p>
+     * Parameters passed to this method is considered to be an instance of
+     * {@link User}. If this isn't so method does nothing and returns false
+     * 
+     * @param obj
+     *            {@link User} to be mapped into database
+     * 
+     */
     @Override
     public boolean save(DomainObject obj) {
 	boolean result = false;
 
 	if (obj != null && obj instanceof User) {
 	    if (!obj.isPersistent()) {
-		result = insert(obj);
+		result = insertUser(obj);
 	    } else {
-		// update record inside database
-		User user = (User) obj;
-
+		result = updateUser(obj);
 	    }
 	}
 
 	return result;
     }
 
-    private boolean insert(DomainObject obj) {
+    private boolean updateUser(DomainObject obj) {
+	boolean result = false;
+
+	if (obj != null && obj instanceof User) {
+	    User user = (User) obj;
+
+	    PreparedStatement pr = null;
+
+	    try {
+		Connection conn = db.getConnection();
+
+		pr = conn
+			.prepareStatement("update users set real_name=?, job=?,"
+				+ "position=?, birthday=?, comments=? "
+				+ "where id=?");
+		pr.setString(1, user.getRealName());
+		pr.setString(2, user.getJob());
+		pr.setString(3, user.getPosition());
+		pr.setDate(4, DateConverter.Convert(user.getBirthday()));
+		pr.setString(5, user.getComments());
+		pr.setLong(6, user.getID());
+
+		int rows_affected = pr.executeUpdate();
+
+		if (rows_affected == 1) {
+		    result = true;
+		}
+
+	    } catch (Exception e) {
+		e.printStackTrace();
+	    } finally {
+		db.Cleanup(pr);
+	    }
+	}
+
+	return result;
+    }
+
+    private boolean insertUser(DomainObject obj) {
 	boolean result = false;
 
 	if (obj != null && obj instanceof User) {
