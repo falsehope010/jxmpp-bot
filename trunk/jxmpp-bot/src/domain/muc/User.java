3f20b2fa6efc32f0b48fb65e6e12c8361c335294
ternovich.work <ternovich.work@3b9d0e60-468d-11de-a2ae-350cbd0950bb>
2009-07-24 20:37:41 +0000

    added javaDoc for User/Room/UserPermissions Modified RoomMapper unit testing Changed documentation

--- trunk/jxmpp-bot/src/domain/muc/User.java
+++ trunk/jxmpp-bot/src/domain/muc/User.java
@@ -4,8 +4,21 @@ import java.util.Date;
 
 import domain.DomainObject;
 
+/**
+ * Represents bot's user. Stores user information and details.
+ * 
+ * @see UserPermissions
+ * @author tillias
+ * 
+ */
 public class User extends DomainObject {
 
+    /**
+     * Creates new user and sets all its informational fields to null.
+     * <p>
+     * User might not want to specify any details, so default constructor is
+     * avaliable for this case
+     */
     public User() {
 	// default constructor avaliable
     }
@@ -19,42 +32,97 @@ public class User extends DomainObject {
 	this.comments = comments;
     }
 
+    /**
+     * Gets user real name
+     * 
+     * @return User real name
+     */
     public String getRealName() {
 	return realName;
     }
 
+    /**
+     * Gets user job
+     * 
+     * @return User job
+     */
     public String getJob() {
 	return job;
     }
 
+    /**
+     * Gets user job position
+     * 
+     * @return User job position
+     */
     public String getPosition() {
 	return position;
     }
 
+    /**
+     * Gets user birthday
+     * 
+     * @return User birthday
+     */
     public Date getBirthday() {
 	return birthday;
     }
 
+    /**
+     * Gets user comments
+     * 
+     * @return User comments
+     */
     public String getComments() {
 	return comments;
     }
 
+    /**
+     * Sets user real name. Parameter can be null
+     * 
+     * @param realName
+     *            User real name
+     */
     public void setRealName(String realName) {
 	this.realName = realName;
     }
 
+    /**
+     * Set user job. Parameter can be null
+     * 
+     * @param job
+     *            User job
+     */
     public void setJob(String job) {
 	this.job = job;
     }
 
+    /**
+     * Sets user job position
+     * 
+     * @param position
+     *            User job position
+     */
     public void setPosition(String position) {
 	this.position = position;
     }
 
+    /**
+     * Sets user birthday. Parameter can be null reference
+     * 
+     * @param birthday
+     *            User birthday
+     */
     public void setBirthday(Date birthday) {
 	this.birthday = birthday;
     }
 
+    /**
+     * Sets user comments. Parameter can be null reference
+     * 
+     * @param comments
+     *            User comments
+     */
     public void setComments(String comments) {
 	this.comments = comments;
     }
