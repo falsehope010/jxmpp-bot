ab049158d6a4930b34d68d8f0b4d83a88c1e9453
ternovich.work <ternovich.work@3b9d0e60-468d-11de-a2ae-350cbd0950bb>
2009-08-11 19:49:30 +0000

    Update smack libraries. SASL Auth now works fine

--- jxmpp-bot/src/Main.java
+++ jxmpp-bot/src/Main.java
@@ -1,12 +1,6 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.ChatManager;
 import org.jivesoftware.smack.ConnectionConfiguration;
-import org.jivesoftware.smack.SASLAuthentication;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
 
@@ -16,49 +10,7 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-
-	Connection conn = null;
-
-	try {
-	    conn = DriverManager
-		    .getConnection("jdbc:derby:derbyDB;create=true");
-
-	    PreparedStatement pr = conn
-		    .prepareStatement("insert into test_table(name,surname) values(?,?)");
-
-	    conn.setAutoCommit(false);
-
-	    for (int i = 0; i < 100000; ++i) {
-		pr.setString(1, Integer.toString(i) + "AAAAAAAAAAAAAAA");
-		pr.setString(2, "12312312313212313123123123");
-
-		pr.executeUpdate();
-
-	    }
-
-	    conn.commit();
-
-	    conn.setAutoCommit(true);
-
-	    pr.close();
-
-	} catch (SQLException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
-
-	try {
-	    if (conn != null)
-		conn.close();
-	} catch (SQLException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
-
-	try {
-	    DriverManager.getConnection("jdbc:derby:;shutdown=true");
-	} catch (SQLException e) {
-	}
+	XmppConnect();
     }
 
     protected static void XmppConnect() {
@@ -69,7 +21,7 @@ public class Main {
 
 	    conn.connect();
 
-	    SASLAuthentication.supportSASLMechanism("PLAIN", 0);
+	    // SASLAuthentication.supportSASLMechanism("PLAIN", 0);
 
 	    conn.login("tillias", "DJ!u[Fc0i5@Z-13FNKK{Ykqj", "test");
 
