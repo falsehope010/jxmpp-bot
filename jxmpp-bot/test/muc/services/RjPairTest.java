65cc292dabea83d00e37e42aae1d81986283f311
ternovich.work <ternovich.work@3b9d0e60-468d-11de-a2ae-350cbd0950bb>
2009-08-13 03:03:02 +0000

    Minor changes

--- jxmpp-bot/test/muc/services/RjPairTest.java
+++ jxmpp-bot/test/muc/services/RjPairTest.java
@@ -1,4 +1,5 @@
 package muc.services;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -10,13 +11,12 @@ public class RjPairTest {
     @Test
     public void testHashCode() {
 
-	String jabberID = "john_doe@xmpp.org";
-	String roomName = "conference@conference.xmpp.org";
-
 	final int counter = 100;
 
-	RjPair p1 = new RjPair(jabberID, roomName);
-	RjPair p2 = new RjPair(jabberID, roomName);
+	RjPair p1 = new RjPair("john_doe@xmpp.org",
+		"conference@conference.xmpp.org");
+	RjPair p2 = new RjPair("john_doe@xmpp.org",
+		"conference@conference.xmpp.org");
 
 	for (int i = 0; i < counter; ++i) {
 	    assertEquals(p1.hashCode(), p2.hashCode());
@@ -47,10 +47,10 @@ public class RjPairTest {
 
 	RjPair p1 = new RjPair(jabberID, roomName);
 
-	// reflexivity
+	// reflexiveness
 	assertTrue(p1.equals(p1));
 
-	// simm
+	// symmetric property
 	RjPair p2 = new RjPair(jabberID, roomName);
 	assertTrue(p1.equals(p2));
 	assertTrue(p2.equals(p1));
