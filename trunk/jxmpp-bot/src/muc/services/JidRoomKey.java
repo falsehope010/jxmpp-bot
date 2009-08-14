4b132fbf3daf1c0b692f7a7caf375f1fbf117cd9
ternovich.work <ternovich.work@3b9d0e60-468d-11de-a2ae-350cbd0950bb>
2009-08-14 13:05:37 +0000

    Small refactoring of JidRoomKey

--- trunk/jxmpp-bot/src/muc/services/JidRoomKey.java
+++ trunk/jxmpp-bot/src/muc/services/JidRoomKey.java
@@ -1,6 +1,6 @@
 package muc.services;
 
-import java.lang.reflect.Array;
+import utils.StringHasher;
 
 /**
  * Stores jabberID and roomName. Both fields are stored as {@link String}
@@ -12,9 +12,6 @@ import java.lang.reflect.Array;
  * 
  */
 public class JidRoomKey {
-
-    protected static final int SEED = 23;
-    protected static final int fODD_PRIME_NUMBER = 37;
 
     public JidRoomKey(String jabberID, String roomName) {
 	this.jabberID = jabberID;
@@ -45,48 +42,14 @@ public class JidRoomKey {
     @Override
     public int hashCode() {
 	if (fHashCode == 0) {
-	    int result = JidRoomKey.SEED;
-	    result = hash(result, jabberID);
-	    result = hash(result, roomName);
+	    int result = StringHasher.SEED;
+	    result = StringHasher.hashString(result, jabberID);
+	    result = StringHasher.hashString(result, roomName);
 
 	    fHashCode = result;
 	}
 
 	return fHashCode;
-    }
-
-    private int hash(int aSeed, Object aObject) {
-
-	int result = aSeed;
-	if (aObject == null) {
-	    result = hash(result, 0);
-	} else if (!isArray(aObject)) {
-	    result = hash(result, aObject.hashCode());
-	} else {
-	    int length = Array.getLength(aObject);
-	    for (int idx = 0; idx < length; ++idx) {
-		Object item = Array.get(aObject, idx);
-		// recursive call!
-		result = hash(result, item);
-	    }
-	}
-	return result;
-    }
-
-    private int hash(int aSeed, int aInt) {
-	/*
-	 * Implementation Note Note that byte and short are handled by this
-	 * method, through implicit conversion.
-	 */
-	return firstTerm(aSeed) + aInt;
-    }
-
-    private boolean isArray(Object aObject) {
-	return aObject.getClass().isArray();
-    }
-
-    private int firstTerm(int aSeed) {
-	return fODD_PRIME_NUMBER * aSeed;
     }
 
     final String jabberID;
