4668abfc8ba8b71691efb110674a06d754832323
ternovich.work <ternovich.work@3b9d0e60-468d-11de-a2ae-350cbd0950bb>
2009-11-02 14:37:12 +0000

    Started implementing VisitService tests

--- trunk/jxmpp-bot/test/muc/services/VisitServiceTest.java
+++ trunk/jxmpp-bot/test/muc/services/VisitServiceTest.java
@@ -1,21 +1,99 @@
 package muc.services;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 import base.DatabaseBaseTest;
+import database.Database;
+import domain.muc.Room;
+import domain.muc.User;
+import domain.muc.UserPermissions;
+import exceptions.DatabaseNotConnectedException;
 
 public class VisitServiceTest extends DatabaseBaseTest {
 
-    @Test
-    public void testVisitService() {
-	fail("Not yet implemented");
+    @Test(expected = NullPointerException.class)
+    public void testVisitServiceNullDatabase() throws Exception {
+	VisitService service = new VisitService(null);
+	assertNull(service);
+    }
+
+    @Test(expected = DatabaseNotConnectedException.class)
+    public void testVisitServiceDisconnectedDb() throws Exception {
+	Database db = prepareDatabase();
+	db.disconnect();
+
+	assertNotNull(db);
+	assertTrue(!db.isConnected());
+
+	VisitService service = new VisitService(db);
+	assertNull(service);
     }
 
     @Test
-    public void testStartVisit() {
-	fail("Not yet implemented");
+    public void testVisitService() throws Exception {
+	Database db = prepareDatabase();
+	VisitService service = new VisitService(db);
+	assertNotNull(service);
+
+	db.disconnect();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testStartVisitNullPermissions() throws Exception {
+	Database db = prepareDatabase();
+	VisitService service = new VisitService(db);
+	assertNotNull(service);
+
+	service.startVisit(null);
+
+	db.disconnect();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testStartVisitNotPersistentPermissions() throws Exception {
+	Database db = prepareDatabase();
+	VisitService service = new VisitService(db);
+	assertNotNull(service);
+
+	UserPermissions permissions = new UserPermissions(new User(), new Room(
+		"testRoom@xmpp.org"), "testUser@xmpp.org");
+
+	service.startVisit(permissions);
+
+	db.disconnect();
+    }
+
+    @Test
+    public void testStartVisit() throws Exception {
+	Database db = prepareDatabase();
+	VisitService service = new VisitService(db);
+	assertNotNull(service);
+
+	assertTruncateDependentTables(db);
+
+	User user = new User();
+	user.mapperSetID(1);
+	user.mapperSetPersistence(true);
+
+	Room room = new Room("testRoom@xmpp.org");
+	room.mapperSetID(1);
+	room.mapperSetPersistence(true);
+
+	UserPermissions permissions = new UserPermissions(user, room,
+		"testUser@xmpp.org");
+	permissions.mapperSetID(1);
+	permissions.mapperSetPersistence(true);
+
+	service.startVisit(permissions);
+
+	// verify against database
+
+	fail("Not yet implementd");
     }
 
     @Test
@@ -28,4 +106,8 @@ public class VisitServiceTest extends DatabaseBaseTest {
 	fail("Not yet implemented");
     }
 
+    private void assertTruncateDependentTables(Database db) {
+	assertTrue(db.truncateTable("permissions"));
+	assertTrue(db.truncateTable("visits"));
+    }
 }
