26ffd4547a9483d14e2e6e614f70bce9982dcd47
ternovich.work <ternovich.work@3b9d0e60-468d-11de-a2ae-350cbd0950bb>
2009-11-04 19:06:03 +0000

    Fake test

---
+++ trunk/jxmpp-bot/test/muc/services/ChatMessageServiceTest.java
@@ -0,0 +1,33 @@
+package muc.services;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class ChatMessageServiceTest {
+
+    @Test
+    public void testChatMessageService() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testFlush() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testSave() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testMapperStateRestored() {
+	fail("Not yet implemented");
+
+	/*
+	 * This test should test that flush() method restores auto-commit state.
+	 * Various initial positions of mapper should be tested
+	 */
+    }
+}
