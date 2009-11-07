f203017ba4d93c6c470207ea7d70a7b8a4ad2c5c
ternovich.work <ternovich.work@3b9d0e60-468d-11de-a2ae-350cbd0950bb>
2009-11-07 06:30:18 +0000

    Minor changes

--- trunk/jxmpp-bot/src/Main.java
+++ trunk/jxmpp-bot/src/Main.java
@@ -2,18 +2,20 @@ import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smackx.muc.MultiUserChat;
+import org.jivesoftware.smackx.muc.ParticipantStatusListener;
 
 public class Main {
 
     /**
      * @param args
+     * @throws InterruptedException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 	XmppConnect();
     }
 
-    protected static void XmppConnect() {
+    protected static void XmppConnect() throws InterruptedException {
 	try {
 	    ConnectionConfiguration configuration = new ConnectionConfiguration(
 		    "jabbus.org", 5222);
@@ -25,13 +27,134 @@ public class Main {
 
 	    conn.login("tillias", "DJ!u[Fc0i5@Z-13FNKK{Ykqj", "test");
 
+	    MultiUserChat chat = null;
+
 	    if (conn.isConnected()) {
 
 		System.out.print("Logged in!\n");
 
-		MultiUserChat chat = new MultiUserChat(conn,
-			"vegatrek@conference.jabber.ru");
-		chat.join("test");
+		chat = new MultiUserChat(conn, "vegatrek@conference.jabber.ru");
+		/*
+		 * chat.addMessageListener(new PacketListener() {
+		 * 
+		 * @Override if (packet instanceof Message) { Message msg =
+		 * (Message) packet; // System.out.println(msg.getBody());
+		 * 
+		 * } else System.out.println(packet.getClass()); } });
+		 */
+
+		// MucCollector manager = new MucCollector();
+		chat
+			.addParticipantStatusListener(new ParticipantStatusListener() {
+
+			    @Override
+			    public void voiceRevoked(String participant) {
+				// TODO Auto-generated method stub
+
+			    }
+
+			    @Override
+			    public void voiceGranted(String participant) {
+				// TODO Auto-generated method stub
+
+			    }
+
+			    @Override
+			    public void ownershipRevoked(String participant) {
+				// TODO Auto-generated method stub
+
+			    }
+
+			    @Override
+			    public void ownershipGranted(String participant) {
+				// TODO Auto-generated method stub
+
+			    }
+
+			    @Override
+			    public void nicknameChanged(String participant,
+				    String newNickname) {
+				// TODO Auto-generated method stub
+
+			    }
+
+			    @Override
+			    public void moderatorRevoked(String participant) {
+				// TODO Auto-generated method stub
+
+			    }
+
+			    @Override
+			    public void moderatorGranted(String participant) {
+				// TODO Auto-generated method stub
+
+			    }
+
+			    @Override
+			    public void membershipRevoked(String participant) {
+				// TODO Auto-generated method stub
+
+			    }
+
+			    @Override
+			    public void membershipGranted(String participant) {
+				// TODO Auto-generated method stub
+
+			    }
+
+			    @Override
+			    public void left(String participant) {
+				System.out.println("Left: " + participant);
+
+			    }
+
+			    @Override
+			    public void kicked(String participant,
+				    String actor, String reason) {
+				// TODO Auto-generated method stub
+
+			    }
+
+			    @Override
+			    public void joined(String participant) {
+				System.out.println("Joined: " + participant);
+
+			    }
+
+			    @Override
+			    public void banned(String participant,
+				    String actor, String reason) {
+				// TODO Auto-generated method stub
+
+			    }
+
+			    @Override
+			    public void adminRevoked(String participant) {
+				// TODO Auto-generated method stub
+
+			    }
+
+			    @Override
+			    public void adminGranted(String participant) {
+				// TODO Auto-generated method stub
+
+			    }
+			});
+
+		chat.join("DigitalSoul");
+
+		/*
+		 * for (Affiliate a : chat.getOwners()) {
+		 * chat.sendMessage("Owner: " + a.getJid()); }
+		 */
+
+		/*
+		 * chat.sendMessage("Hello this is me!");
+		 * 
+		 * Message msg = new Message(
+		 * "vegatrek@conference.jabber.ru/tillias");
+		 * msg.setBody("This is PM"); chat.sendMessage(msg);
+		 */
 
 		/*
 		 * ChatManager chatManager = conn.getChatManager();
@@ -47,22 +170,17 @@ public class Main {
 		System.out.print("Can't login\n");
 	    }
 
-	    boolean flag = false;
-
-	    while (!flag) {
-		try {
-		    Thread.sleep(1000);
-		} catch (InterruptedException e) {
-		    e.printStackTrace();
-		}
+	    while (true) {
+		Thread.sleep(100);
 	    }
 
-	    conn.disconnect();
+	    // chat.leave();
+	    // conn.disconnect();
 
 	    // System.out.print(conn.isConnected());
 	} catch (XMPPException ex) {
 	    System.out.print(ex.getMessage());
+	    System.out.print(ex.getStackTrace());
 	}
     }
-
 }
