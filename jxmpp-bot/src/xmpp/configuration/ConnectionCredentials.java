d6f4648795020c1bbd46a267d7b8d56f9fbfc025
ternovich.work <ternovich.work@3b9d0e60-468d-11de-a2ae-350cbd0950bb>
2009-11-19 13:08:59 +0000

    Added several javaDocs

--- jxmpp-bot/src/xmpp/configuration/ConnectionCredentials.java
+++ jxmpp-bot/src/xmpp/configuration/ConnectionCredentials.java
@@ -13,6 +13,8 @@ public class ConnectionCredentials {
 
     /**
      * Creates new instance of credentials using default server port
+     * 
+     * @see #DEFAULT_SERVER_PORT
      */
     public ConnectionCredentials() {
 	setPort(DEFAULT_SERVER_PORT);
@@ -33,6 +35,13 @@ public class ConnectionCredentials {
 	setOwnerJID(c.getOwnerJID());
     }
 
+    /**
+     * Performs basic validation of credentials. Checks that port number is
+     * positive, text fields aren't null and owner JID is valid jabber
+     * identifier (e.g. has the form of name@server.domain)
+     * 
+     * @return True validation has succeeded, false otherwise
+     */
     public boolean validate() {
 	boolean result = getPort() > 0;
 	result &= getNick() != null;
@@ -65,47 +74,112 @@ public class ConnectionCredentials {
 	return sb.toString();
     }
 
+    /**
+     * Gets nick name (e.g. login) which system will use while authorizing on
+     * remote xmpp server
+     * 
+     * @return Nick name (login)
+     */
     public String getNick() {
 	return nick;
     }
 
+    /**
+     * Sets nick name (e.g. login) which system will use while authorizing on
+     * remote xmpp server
+     * 
+     * @param nick
+     *            New nick name (login)
+     */
     public void setNick(String nick) {
-	this.nick = new String(nick);
+	this.nick = nick;
     }
 
+    /**
+     * Gets password which system will use while authorizing on remote xmpp
+     * server
+     * 
+     * @return Password
+     */
     public String getPassword() {
 	return password;
     }
 
+    /**
+     * Sets password which system will use while authorizing on remote xmpp
+     * server
+     * 
+     * @param password
+     *            New password
+     */
     public void setPassword(String password) {
-	this.password = new String(password);
+	this.password = password;
     }
 
+    /**
+     * Gets remote xmpp server host name
+     * 
+     * @return Server host name
+     */
     public String getServer() {
 	return server;
     }
 
+    /**
+     * Sets remote xmpp server host name
+     * 
+     * @param server
+     *            New server host name
+     */
     public void setServer(String server) {
-	this.server = new String(server);
+	this.server = server;
     }
 
+    /**
+     * Gets remote xmpp server port
+     * 
+     * @return Server port
+     */
     public int getPort() {
 	return port;
     }
 
+    /**
+     * Sets remote xmpp server port
+     * 
+     * @param port
+     *            New server port
+     */
     public void setPort(int port) {
 	this.port = port;
     }
 
+    /**
+     * Gets jabber identifier of system owner (administrator) that will be able
+     * to perform specifical tasks
+     * 
+     * @return Owner jabber identifier
+     */
     public String getOwnerJID() {
 	return ownerJID;
     }
 
+    /**
+     * Sets jabber identifier of system owner (administrator) that will be able
+     * to perform specifical tasks
+     * 
+     * @param ownerJID
+     *            New owner jabber identifier
+     */
     public void setOwnerJID(String ownerJID) {
-	this.ownerJID = new String(ownerJID);
+	this.ownerJID = ownerJID;
     }
 
+    /**
+     * Default xmpp server port. Currently is 5222
+     */
     public static final int DEFAULT_SERVER_PORT = 5222;
+
     static final Pattern regex = Pattern
 	    .compile("^([\\p{Print})&&[^@]])+@(([\\p{Print})&&[^@.]])+[.])+\\w+");
 
--- jxmpp-bot/src/xmpp/messaging/Message.java
+++ jxmpp-bot/src/xmpp/messaging/Message.java
@@ -2,7 +2,70 @@ package xmpp.messaging;
 
 import java.util.Date;
 
+/**
+ * Base class for all XMPP messages. Subclasses must be <b>immutable</b>
+ * 
+ * @author tillias
+ * 
+ */
 public abstract class Message {
+
+    /**
+     * Sole constructor for invocation by subclasses. Sets message date to
+     * current one
+     * 
+     * @param sender
+     *            Message sender
+     * @param recipient
+     *            Message recipient
+     * @param text
+     *            Message text
+     */
+    public Message(ParticipantInfo sender, ParticipantInfo recipient,
+	    String text) {
+	if (sender == null || recipient == null)
+	    throw new NullPointerException("Sender or recipient can't be null");
+    }
+
+    /**
+     * Gets sender of this message
+     * 
+     * @return Sender of this message
+     * @see ParticipantInfo
+     */
+    public ParticipantInfo getSender() {
+	return new ParticipantInfo(sender);
+    }
+
+    /**
+     * Gets recipient of this message
+     * 
+     * @return Recipient of this message
+     * @see ParticipantInfo
+     */
+    public ParticipantInfo getRecipient() {
+	return new ParticipantInfo(recipient);
+    }
+
+    /**
+     * Gets timestamp of this message (e.g. timestamp when this message was
+     * created)
+     * 
+     * @return Timestamp of this message
+     */
+    public Date getTimestamp() {
+	return new Date(timestamp.getTime());
+    }
+
+    /**
+     * Gets text block of this message. Text block can be null
+     * 
+     * @return Text block of this message
+     */
+    public String getText() {
+	return text;
+    }
+
     ParticipantInfo sender;
     ParticipantInfo recipient;
     Date timestamp;
