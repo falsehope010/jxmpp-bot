83c815cccbbab458860cd7f242517e1bcb2546e3
ternovich.work <ternovich.work@3b9d0e60-468d-11de-a2ae-350cbd0950bb>
2009-11-21 13:18:04 +0000

    Added xmpp test stub

--- jxmpp-bot/test/suites/XmppTestsSuite.java
+++ jxmpp-bot/test/suites/XmppTestsSuite.java
@@ -8,11 +8,12 @@ import xmpp.listeners.PrivateMessageListenerTest;
 import xmpp.messaging.ParticipantInfoTest;
 import xmpp.messaging.PrivateMessageTest;
 import xmpp.queue.MessageQueueTest;
+import xmpp.utils.PresenceProcessorTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses( { ConfigurationTest.class, MessageQueueTest.class,
-	ParticipantInfoTest.class, PrivateMessageTest.class,
-	PrivateMessageListenerTest.class })
+	ParticipantInfoTest.class, PresenceProcessorTest.class,
+	PrivateMessageTest.class, PrivateMessageListenerTest.class })
 public class XmppTestsSuite {
     // nothing goes here
 }
---
+++ jxmpp-bot/test/xmpp/utils/PresenceProcessorTest.java
@@ -0,0 +1,14 @@
+package xmpp.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PresenceProcessorTest {
+
+    @Test
+    public void testRequestJabberID() {
+	fail("Not yet implemented"); // TODO
+    }
+
+}
