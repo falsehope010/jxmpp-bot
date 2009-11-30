629dfee7d431d3e74d043ccb14063a6d1f53cf80
ternovich.work <ternovich.work@3b9d0e60-468d-11de-a2ae-350cbd0950bb>
2009-11-30 18:48:36 +0000

    Implemented Value Object for ParticipantInfo. Added unit tests

--- trunk/jxmpp-bot/src/xmpp/messaging/domain/ParticipantInfo.java
+++ trunk/jxmpp-bot/src/xmpp/messaging/domain/ParticipantInfo.java
@@ -1,8 +1,11 @@
 package xmpp.messaging.domain;
 
+import utils.HashUtil;
+
 /**
  * Stores information about person who is participating in private conversation
- * or in a group chat. Class is <b>immutable</b>
+ * or in a group chat. Class is <b>immutable</b>. Implements Value Object
+ * pattern.
  * 
  * @author tillias
  * 
@@ -77,6 +80,32 @@ public final class ParticipantInfo {
 	return sb.toString();
     }
 
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+
+	if (!(obj instanceof ParticipantInfo))
+	    return false;
+
+	ParticipantInfo pi = (ParticipantInfo) obj;
+
+	return pi.getAdress().equals(getAdress())
+		&& pi.getJabberID().equals(getJabberID());
+
+    }
+
+    @Override
+    public int hashCode() {
+	if (fHashCode == 0) {
+	    fHashCode = HashUtil.SEED;
+	    fHashCode ^= HashUtil.hashString(fHashCode, jabberID);
+	    fHashCode ^= HashUtil.hashString(fHashCode, adress);
+	}
+	return fHashCode;
+    }
+
     String jabberID;
     String adress;
+    int fHashCode;
 }
