850885f5a7f6a1b35ade1e366a97e3fe8cb945c3
ternovich.work <ternovich.work@3b9d0e60-468d-11de-a2ae-350cbd0950bb>
2009-11-30 18:48:36 +0000

    Implemented Value Object for ParticipantInfo. Added unit tests

--- jxmpp-bot/test/xmpp/messaging/ParticipantInfoTest.java
+++ jxmpp-bot/test/xmpp/messaging/ParticipantInfoTest.java
@@ -1,6 +1,7 @@
 package xmpp.messaging;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
@@ -33,6 +34,45 @@ public class ParticipantInfoTest {
 	assertEquals(base.getJabberID(), dest.getJabberID());
 	assertTrue(base.equals(dest));
 	assertEquals(base.hashCode(), dest.hashCode());
+    }
+
+    @Test
+    public void testEquals() {
+	ParticipantInfo a = new ParticipantInfo("jid", "adress");
+	ParticipantInfo b = new ParticipantInfo("jid", "adress");
+
+	assertTrue(a.equals(a));
+
+	assertTrue(a.equals(b));
+	assertTrue(b.equals(a));
+
+	ParticipantInfo c = new ParticipantInfo("jid", "adress");
+	assertTrue(a.equals(b));
+	assertTrue(b.equals(c));
+	assertTrue(a.equals(c));
+
+	assertFalse(a.equals(null));
+	assertFalse(a.equals(new Object()));
+
+	ParticipantInfo d = new ParticipantInfo("jid1", "adress");
+	assertFalse(a.equals(d));
+
+	ParticipantInfo e = new ParticipantInfo("jid", "adress1");
+	assertFalse(a.equals(e));
+    }
+
+    @Test
+    public void testHashCode() {
+	ParticipantInfo a = new ParticipantInfo("jid", "adress");
+	ParticipantInfo b = new ParticipantInfo("jid", "adress");
+	assertEquals(a.hashCode(), b.hashCode());
+	assertEquals(b.hashCode(), a.hashCode());
+
+	ParticipantInfo c = new ParticipantInfo("jid1", "adress");
+	assertFalse(a.hashCode() == c.hashCode());
+
+	ParticipantInfo d = new ParticipantInfo("jid", "adress1");
+	assertFalse(a.hashCode() == d.hashCode());
     }
 
 }
