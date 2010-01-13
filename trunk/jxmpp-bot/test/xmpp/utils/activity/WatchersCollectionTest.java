9dbc7267e21629d8fce9ae137ac82a2e6a591e1b
ternovich.work <ternovich.work@3b9d0e60-468d-11de-a2ae-350cbd0950bb>
2010-01-13 11:04:45 +0000

    Implemented AppearanceMessage unit test

---
+++ trunk/jxmpp-bot/test/xmpp/utils/activity/WatchersCollectionTest.java
@@ -0,0 +1,44 @@
+package xmpp.utils.activity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WatchersCollectionTest {
+
+    @Test
+    public void testWatchersCollection() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAdd() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testRemove() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testStart() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testStop() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetLog() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetPollTimeout() {
+	fail("Not yet implemented");
+    }
+
+}
